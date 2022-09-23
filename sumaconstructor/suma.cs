a485ccf57160a41c0bbd3ea80d807450b33ae574
Angel González <anngeeldavid@gmail.com>
2022-09-22 19:50:43 -0600

    Agregar archivos de proyecto.

---
+++ sumaconstructor/suma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumaconstructor
+{
+    class suma
+    {
+        public double V1;
+        public double V2;
+
+        public suma()
+        {
+        }
+
+        public suma(double v1, double v2)
+        {
+            V1 = v1;
+            V2 = v2;
+        }
+        public double sumar()
+        {
+            return V1 + V2;
+        }
+    }
+}
